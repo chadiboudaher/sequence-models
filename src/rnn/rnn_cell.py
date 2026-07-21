052b167582b75bdb2bb96d4add9ab7d7892143b7
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-21 19:30:47 +0300

    feat: initialize RNN cell paramters

---
+++ src/rnn/rnn_cell.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+
+class RNNCell:
+    def __init__(self, input_size, hidden_size, output_size):
+        """
+        Initialize RNN cell parameters
+
+        Args:
+            input_size: Dimension of input features.
+            hidden_size: Dimension of hidden state.
+            output_size: Dimension of output.
+        """
+
+        self.U = np.random(hidden_size, input_size) * 0.01
+        self.W = np.random(hidden_size, hidden_size) * 0.01
+        self.V = np.random(hidden_size, output_size) * 0.01
+
+        self.b = np.zeros((hidden_size, 1))
+        self.c = np.zeros((output_size, 1))
+
+        
