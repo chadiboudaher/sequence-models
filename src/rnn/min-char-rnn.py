29231a620ac2bcaae763b90846a362ec1ac150d6
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-22 18:38:51 +0300

    feat: add forward pass for RNN model

--- src/rnn/min-char-rnn.py
+++ src/rnn/min-char-rnn.py
@@ -25,3 +25,25 @@ Whh = np.random.randn(hidden_size, hidden_size) * 0.01
 Why = np.random.randn(vocab_size, hidden_size) * 0.01
 bh = np.zeros((hidden_size, 1))
 by = np.zeros((vocab_size, 1))
+
+def lossFun(inputs, targets, hprev):
+    """
+    inputs, targets are both lists of integers
+    hprev is Hx1 array of initial hidden state
+    return loss, gradient on model parameters, and last hidden state
+    """
+
+    xs, hs, ys, ps = {}, {}, {}, {}
+
+    hs[-1] = np.copy(hprev)
+    loss = 0
+    # Forward pass
+    for t in range(len(inputs)):
+        xs[t] = np.zeros((vocab_size, 1))
+        xs[t][inputs[t]] = 1
+        hs[t] = np.tanh(np.dot(Wxh, xs[t]) + 
+                        np.dot(Whh, hs[t-1]) + 
+                        bh)
+        ys[t] = np.dot(Why, hs[t]) + by
+        ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))
+        loss += -np.log(ps[t][targets[t], 0])
