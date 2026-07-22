13325ea9d9cc267b5c835ff7d258cef7054ddd22
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-22 18:22:45 +0300

    feat: add model parameters used in RNN model

--- src/rnn/min-char-rnn.py
+++ src/rnn/min-char-rnn.py
@@ -18,3 +18,10 @@ ix_to_char = { i:ch for i, ch in enumerate(chars)}
 hidden_size = 1000 # size of hidden layer of neurons
 seq_length = 25 # number of steps to unroll the RNN for
 learning_rate = 1e-1
+
+# model paramters
+Wxh = np.random.randn(hidden_size, vocab_size) * 0.01
+Whh = np.random.randn(hidden_size, hidden_size) * 0.01
+Why = np.random.randn(vocab_size, hidden_size) * 0.01
+bh = np.zeros((hidden_size, 1))
+by = np.zeros((vocab_size, 1))
