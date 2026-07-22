b857df86b516b0a9fbc4eb7801a779c9f514263b
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-22 18:18:12 +0300

    feat: set hyperparameter

--- src/rnn/min-char-rnn.py
+++ src/rnn/min-char-rnn.py
@@ -13,3 +13,8 @@ print(f"data has {data_size} characters, {vocab_size} unique.")
 # Convert to integer before feeding it into a NN
 char_to_ix = { ch:i for i, ch in enumerate(chars)}
 ix_to_char = { i:ch for i, ch in enumerate(chars)}
+
+# Hyperparameter
+hidden_size = 1000 # size of hidden layer of neurons
+seq_length = 25 # number of steps to unroll the RNN for
+learning_rate = 1e-1
